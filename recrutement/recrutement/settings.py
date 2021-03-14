7737b49961563cb2c93d40de3261f569dc7f2802
Thevaleentiin <mikado93220@gmail.com>
2021-03-14 20:45:12 +0100

    fix commit before

--- recrutement/recrutement/settings.py
+++ recrutement/recrutement/settings.py
@@ -122,7 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = 'static/'
-#STATIC_ROOT = (os.path.join(SITE_ROOT, 'static_files/'))
 import os
 SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 STATICFILES_DIRS = (
