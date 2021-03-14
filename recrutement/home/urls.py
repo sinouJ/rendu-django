415f80caa6142ad626332c31b443276d017a3018
sinouJ <sinoujordan@gmail.com>
2021-03-14 22:22:18 +0100

    auth

--- recrutement/home/urls.py
+++ recrutement/home/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index_view, name="index_view")
+    path('', views.index_view, name="index_view"),
+    path('signup/', views.user_signup, name="user_signup"),
+    path('logout/', views.user_logout, name="user_logout"),
+    path('login/', views.user_login, name="user_login")
 ]
--- recrutement/home/views.py
+++ recrutement/home/views.py
@@ -1,4 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout, authenticate, login
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 def index_view(request):
     return render(request, 'home/home.html')
+
+def user_login(request):
+
+    if(request.method == 'POST'):
+        form = AuthenticationForm(request.POST)
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('index_view')
+        
+        else:
+            return 'TODO error log message'
+
+    else: 
+        form = AuthenticationForm()
+
+    return render(request, 'home/login.html', {'form': form})
+
+def user_signup(request):
+    page_title = 'Inscription'
+
+    if(request.method == 'POST'):
+        form = UserCreationForm(request.POST)
+
+        if(form.is_valid()):
+            form.save()
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'home/signup.html', {'form': form})
+
+def user_logout(request):
+    logout(request)
+
+    return redirect('index_view')
