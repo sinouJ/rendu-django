15d6b809e71463933163e106961d518b244e932d
sinouJ <sinoujordan@gmail.com>
2021-03-14 19:14:32 +0100

    display posts

--- recrutement/nos_projets/urls.py
+++ recrutement/nos_projets/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.projets_view, name="projets_view")
+    path('', views.projets_view, name="projets_view"),
+    path('<int:id>/', views.post_view, name='post_view'),
+    path('new/', views.post_new, name='post_new'),
+    path('<int:id>/edit/', views.post_edit, name="post_edit"),
+    path('<int:id>/publish_post/', views.publish_post, name="publish_post"),
+    path('<int:id>/archive_post/', views.archive_post, name="archive_post"),
 ]
--- recrutement/nos_projets/views.py
+++ recrutement/nos_projets/views.py
@@ -1,4 +1,69 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import redirect, get_object_or_404
+from django.utils import timezone
+
+from .forms import PostForm
+from .models import Post
 
 def projets_view(request):
-    return render(request, 'nos_projets/index.html')
+    posts = Post.objects.all()
+    return render(request, 'nos_projets/index.html', {'posts': posts})
+
+def post_view(request, id):
+    post = get_object_or_404(Post, id = id)
+    page_title = post.title + ' | Post'
+    return render(request, 'nos_projets/post_view.html', {'post': post, 'page_title': page_title})
+
+def post_new(request):
+    page_title = 'Créer un post'
+
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+
+        if form.is_valid():
+            post = form.save(commit = False)
+            post.author = request.user
+            post.is_published = False
+            post.save()
+            return redirect('post_view', id=post.id)
+
+    else:
+        form = PostForm()
+        return render(request, 'nos_projets/post_new.html', {'form': form, 'page_title': page_title})
+
+def post_edit(request, id):
+    page_title = 'Éditer un post'
+
+    post = get_object_or_404(Post, id = id)
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance = post)
+
+        if form.is_valid():
+            post = form.save(commit = False)
+            post.author = request.user
+            post.is_published = False
+            post.save()
+            return redirect('post_view', id=post.id)
+
+    else:
+        form = PostForm(instance = post)
+        return render(request, 'nos_projets/post_new.html', {'form': form, 'page_title': page_title})
+
+def publish_post(request, id):
+    post = get_object_or_404(Post, id = id)
+
+    post.is_published = True
+    post.published_date = timezone.now()
+    post.save()
+
+    return redirect('all_posts')
+
+def archive_post(request, id):
+    post = get_object_or_404(Post, id = id)
+
+    post.is_published = False
+    post.save()
+
+    return redirect('all_posts')
