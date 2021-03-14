8e7ce7f341daead0c2818c57dba506ddb4b244be
sinouJ <sinoujordan@gmail.com>
2021-03-14 15:51:42 +0100

    delete recrutement_app

--- recrutement/recrutement_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class RecrutementAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'recrutement_app'
--- recrutement/recrutement_app/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def home_view(request):
-    return render(request, 'recrutement_app/home_view.html', {})
