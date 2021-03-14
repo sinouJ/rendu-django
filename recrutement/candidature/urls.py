e7ef79836d6f1afadbb7e66165c0d1f3a22e63a7
sinouJ <sinoujordan@gmail.com>
2021-03-14 23:34:43 +0100

    candidatures

--- recrutement/candidature/urls.py
+++ recrutement/candidature/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.candidature_view, name="candidature_view"),
-    # path('blabla', views.blabla, name="blabla")
-]
+    path('postcv/', views.post_cv_view, name="post_cv_view"),
+    path('detail_candidature/<int:id>/', views.detail_candidature_view, name="detail_candidature_view")
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- recrutement/candidature/views.py
+++ recrutement/candidature/views.py
@@ -1,23 +1,31 @@
-from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import PostCVForm
+from .models import PostCV
+from django.utils import timezone
 
 def candidature_view(request):
+    candidatures = PostCV.objects.all()
+    return render(request, 'candidature/home.html', {'candidatures': candidatures})
+
+def post_cv_view(request):
     if request.method == 'POST':
         form = PostCVForm(request.POST, request.FILES)
+
         if form.is_valid():
+            cv = form.save(commit = False)
+            cv.author = request.user
+            cv.published_date = timezone.now()
+            cv.fileCV = request.FILES['fileCV']
             form.save()
-            return HttpResponseRedirect('/success/url/')
+            return redirect('candidature_view')
+        
+        else: 
+            return render(request, 'candidature/home.html')
+
     else:
         form = PostCVForm()
-    return render(request, 'candidature/home.html', {'form': form})
+    return render(request, 'candidature/post_cv.html', {'form': form})
 
-def upload_file(request):
-    if request.method == 'POST':
-        form = ModelFormWithFileField(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect('/success/url/')
-        else:
-            form = ModelFormWithFileField()
-        return render(request, 'upload.html', {'form': form})
+def detail_candidature_view(request, id):
+    candidature = get_object_or_404(PostCV, id = id)
+    return render(request, 'candidature/detail_candidature_view.html', {'candidature': candidature})
